250df5d54c765764505605105f65da8404ef11c2
jrtgad <jruiztoledog@gmail.com>
2015-12-01 21:32:04 +0100

    ?

--- proyectogamestore/js/main.js
+++ proyectogamestore/js/main.js
@@ -1,9 +1,15 @@
+var cont = 0, archivos = ["img/Fotos/autopromo/nba16.jpg", "video"];
+
 function events() {
-    $("autopromo").setInterval(changeBanner, 12000);
+    var tiempo = setInterval(changeBanner, 12000);
 }
 
 function changeBanner() {
-    $("autopromo").replaceChild("../img/autopromo/", oldChild);
+    if(cont === archivos.length) {
+    	cont = 0;
+    }
+    $("fotopromo").src = archivos[cont];
+    cont++;
 }
 
 function $(id) {
